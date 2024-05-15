64077380ec4c357d10700593937dbcfe7ce4d58a
kilkennylj <boston19rules@outlook.com>
2024-05-15 13:46:55 -0400

    Added image, other qol

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 import '../styles/HomePage.css';
 
+import liamImage from "../images/liam.jpeg";
+
 function About ()
 {
     return(
         <div className="about">
-            About me
+            <h1 style={{display: 'block'}}>About Me</h1>
+            <div className="aboutImgContainer">
+                <img className="aboutImg" src={liamImage} alt='Liam Kilkenny, creator of this webpage' />
+            </div>
         </div>
     );
 }
