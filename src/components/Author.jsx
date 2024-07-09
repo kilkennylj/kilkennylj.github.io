0658d484894cf758b509b2ad1fbc777eaaa1bc88
kilkennylj <boston19rules@outlook.com>
2024-07-08 20:56:20 -0400

    Added author component

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const Author = () =>
+// If I create a bio, put it in a p to the right of the name and img, make sure those two move left.
+// Image is hardcoded because hygraph kept returning null for author.picture
+
+const Author = ({ author }) =>
 {
     return(
-        <div>
-            
+        <div className="authorContainer">
+            <h2>{author.name}</h2>
+            <img
+                alt={author.name}
+                className="authorImg"
+                src="/images/liam.jpeg"
+            />
         </div>
     )
 }
