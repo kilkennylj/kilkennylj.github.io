094565ddc14a53504a35a1f3f45a043b4aac93a6
kilkennylj <boston19rules@outlook.com>
2024-07-29 14:47:58 -0400

    Added resume to resume page

---
+++ src/components/Resume.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../styles/ResumePage.css';
+
+const Resume = () =>
+{
+    return(
+    <div className="resumeContainer">
+        <iframe src="/assets/Kilkenny_Liam_Resume.pdf" width="100%" height="100%" />
+    </div>
+    );
+}
+
+export default Resume;
